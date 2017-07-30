c7c8aef90ae4105b35a0cf7eed1ac1fab4bc1ccd
pavnguyen <vunguyen.xbt@gmailc.com>
2017-07-30 19:31:37 +0700

    commit

--- make_video.py
+++ make_video.py
@@ -1,54 +1,122 @@
 """
 Script to create the movie
 """
+import os, sys, datetime
+from PIL import Image
+from gtts import gTTS
+from mutagen.mp3 import MP3
+import urllib.request
+from moviepy.editor import *
+import textwrap
+from moviepy.video.tools.drawing import color_gradient
 import numpy as np
 from skimage import transform as tf
-from moviepy.editor import *
-from moviepy import *
-from gtts import gTTS
-import os.path
-from moviepy.video.tools.drawing import color_gradient
+
 
 # RESOLUTION
-w = 1920
-h = 1080
-moviesize = w, h
+W = 1280
+H = 720
+moviesize = W, H
+lang_choice = 'en-us'
+if len(sys.argv) > 1:
+  from newspaper import Article
+  url = sys.argv[1]
+  try:
+    lang_choice = sys.argv[2]
+  except:
+    pass
+  print(lang_choice)
+  article = Article(url)
+else:
+  import newspaper
+  paper = newspaper.build('http://cnn.com', memoize_articles=False)
+  article = paper.articles[0]
+article.download()
+article.parse()
+authors = article.authors
+top_image = article.top_image
+images = article.images
 
-# THE RAW TEXT
-txt = '\n\nHiện trường bít bùng khiến 8 người không kịp thoát Hiện trường bít bùng khiến 8 người không kịp thoát\n\nKhoảng 10h30, sau tiếng nổ lớn từ xưởng sản xuất bánh kẹo nằm ven quốc lộ 32, xã Đức Thượng (Hoài Đức, Hà Nội), khói lửa bốc lên nghi ngút, cao vài chục mét. Hàng chục công nhân bị mắc kẹt sau cánh cổng sắt đóng kín.\n\nCảnh sát phòng cháy chữa cháy cùng nhiều xe cứu hỏa được điều đến hiện trường. Đến khoảng 12h, đám cháy được dập tắt. Một số thi thể được đưa lên xe cứu thương chở tới bệnh viện.\n\nHiện trường vụ nổ. Ảnh: Phạm Dự.\n\nGhi nhận của phóng viên VnExpress, đến 13h55, thêm hai thi thể được đưa ra. Nhà xưởng một tầng, rộng trên 100 m3 cháy trơ trụi. Những tấm tôn lợp mái và tôn bao quanh xưởng nằm ngổn ngang, che chắn hết lối đi.\n\nThoát ra khỏi đám cháy, một nữ công nhân kể sau tiếng nổ lớn, những tấm sắt gần cửa sập xuống, bịt mất lối ra vào duy nhất. Các công nhân chạy dồn vào phía trong, bất lực nhìn lửa càng lúc càng bốc mạnh.\n\nMột nguồn tin cũng cho biết, các nạn nhân dồn vào phía cuối xưởng, "khi cảnh sát phát hiện, tất cả đang trong tư thế ôm nhau, thi thể cháy đen".\n\nNhân chứng kể lại vụ cháy\n\nĐến 15h, theo báo cáo của UBND huyện Hoài Đức, có khoảng 7-8 người tử vong do mắc kẹt trong xưởng, 2 người bị thương được đưa tới Bệnh viện huyện Đan Phượng trong tình trạng hôn mê. Nguyên nhân cháy là xưởng đang sửa chữa, thợ hàn xì làm bắn tia lửa điện vào trần gác xép được ghép bằng xốp.\n\nÔng Bùi Văn Sơn, Phó giám đốc Bệnh viện huyện Đan Phượng cho biết, khoảng 11h30, bệnh viện tiếp nhận 3 nạn nhân vụ cháy thì một người đã tử vong từ trước, hai người còn lại bỏng rất nặng, khoảng độ 3 kèm thêm bỏng hô hấp. Sau khi sơ cứu, chống sốc, các nạn nhân đã được chuyển lên Viện Bỏng Quốc gia.\n\nXưởng bị cháy chỉ có một lối ra vào duy nhất. Ảnh: Phạm Dự.\n\nXưởng bị cháy rộng 170 m2, mặt tiền khoảng 7 m, được lợp tôn, tường gạch, có một gác xép chừng 100 m2 nằm giữa một xưởng sản xuất thép và nhà dân cao 3 tầng. Cơ sở này do ông Nguyễn Văn Được (Phúc Thọ, Hà Nội) thuê đất để sản xuất bánh kem và chocolate.\n\nXưởng cũng là nơi ở của gia đình ông Được cùng một số công nhân.\n\nẢnh: Hiện trường vụ cháy khiến 8 người chết'
+list_images = []
+j = 0
+for i in images:
+  try:
+    j += 1
+    filename = str(j) + '.jpg'
+    urllib.request.urlretrieve(i, 'tmp/' + filename)
+    list_images.append(filename)
+  except:
+    if j > 0:
+      j =- 1
+    continue
 
+print(list_images)
+try:
+  article.nlp()
+except:
+    print('Error NLP')
+    pass
+keywords = article.keywords
+print(keywords)
+
+txt = article.text
+txt = '\n'.join(textwrap.wrap(txt, 55))
 
 # Add blanks
 txt = 10*"\n" + txt + 10*"\n"
 
-if os.path.exists('txt.mp3') is False:
-	tts = gTTS(text=txt, lang='vi')
-	tts.save("txt.mp3")
+#if os.path.exists('txt.mp3') is False:
+tts = gTTS(text=txt, lang=lang_choice)
+tts.save("txt.mp3")
+
+audio = MP3("txt.mp3")
+audio_length = int(audio.info.length)
 
 # CREATE THE TEXT IMAGE
-clip_txt = TextClip(txt,color='white', align='West', fontsize=50, font='NotoSerifTamil-Bold', method='label')
+clip_txt = TextClip(txt,color='white', align='West', fontsize=24, font='NotoSerifTamil-Bold', method='label')
 
 # SCROLL THE TEXT IMAGE BY CROPPING A MOVING AREA
-txt_speed = 27
-fl = lambda gf,t : gf(t)[int(txt_speed*t):int(txt_speed*t)+int(h),:]
+txt_speed = 8
+fl = lambda gf,t : gf(t)[int(txt_speed*t):int(txt_speed*t)+int(H),:]
 moving_txt = clip_txt.fl(fl, apply_to=['mask'])
 
+
 # BACKGROUND IMAGE, DARKENED AT 60%
-background_image = ImageClip('stars.jpg')
-stars_darkened = background_image.fl_image(lambda pic: (0.6*pic).astype('int16'))
+background_image = ImageClip('background.jpg')
+background_darkened = background_image.fl_image(lambda pic: (0.6*pic).astype('int16'))
 
 # COMPOSE THE MOVIE
-#audio = AudioFileClip("hello.mp3")
-background_2 = ImageClip('2.png').set_pos('center').set_duration(10)
+clips = []
+#clips.append(background_image)
+print('Number of images:' + str(len(list_images)))
+list_images_new = []
+for i in range (len(list_images)):
+  filepath = 'tmp/' + list_images[i]
+  size = int(os.stat(filepath).st_size)
+  im=Image.open(filepath)
+  if size > 15000 and int(im.size[0]) > 400 :
+    list_images_new.append(list_images[i])
 
-videoclip = CompositeVideoClip([
-         background_image, background_2,
-         moving_txt.set_pos(('center','bottom'))], moviesize)
 
-videoclip.set_duration(len(txt)/8).write_videofile("videoclip.avi", fps=25,
-  codec='libx264', 
-  audio="txt.mp3",
-  audio_codec='aac', 
-  temp_audiofile='txt.mp3', 
-  remove_temp=True
+timing_duration = int(audio_length/len(list_images_new))
+print('Timing Duration:' + str(timing_duration))
+print('New number of images:' + str(len(list_images_new)))
+for j in range (len(list_images_new)):
+  #print('tmp/'+list_images_new[j])
+  slide = ImageClip('tmp/'+list_images_new[j]).set_duration(timing_duration).set_start(timing_duration * j).set_pos(lambda t: ('center', 50+t) ).crossfadein(.3)
+  clips.append(slide)
+
+#clips.append(moving_txt.set_pos(('center','bottom')))
+#slides = [add_transition(len(clips), x, clip) for x, clip in enumerate(clips)]
+videoclip = CompositeVideoClip(clips, moviesize)
+videoclip.set_duration(audio_length).write_videofile("videoclip_"+ datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ".avi", fps=5, codec='libx264', 
+        audio="txt.mp3", audio_codec='aac', temp_audiofile='txt.mp3', remove_temp=True
 )
+
+# DELETE TEMPORY FILES IMAGES
+filelist = [ f for f in os.listdir("tmp/") if f.endswith(".jpg") ]
+for f in filelist:
+  try:
+    os.remove(f)
+  except:
+    continue
