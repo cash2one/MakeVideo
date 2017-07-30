cc9768f078b6eec007b317ff55f390b199584376
pavnguyen <vunguyen.xbt@gmailc.com>
2017-07-30 23:29:29 +0700

    ok

--- make_video.py
+++ make_video.py
@@ -1,7 +1,10 @@
 """
 Script to create the movie
 """
-import os, sys, datetime
+import os
+import shutil
+import sys
+import datetime
 from PIL import Image
 from gtts import gTTS
 from mutagen.mp3 import MP3
@@ -25,7 +28,7 @@ if len(sys.argv) > 1:
     lang_choice = sys.argv[2]
   except:
     pass
-  print(lang_choice)
+  print('Language: ' + lang_choice)
   article = Article(url)
 else:
   import newspaper
@@ -34,6 +37,7 @@ else:
 article.download()
 article.parse()
 authors = article.authors
+title = article.title
 top_image = article.top_image
 images = article.images
 
@@ -50,30 +54,34 @@ for i in images:
       j =- 1
     continue
 
-print(list_images)
+#print(list_images)
 try:
   article.nlp()
 except:
     print('Error NLP')
     pass
 keywords = article.keywords
-print(keywords)
 
-txt = article.text
-txt = '\n'.join(textwrap.wrap(txt, 55))
+txt_nowrap = article.text
+print('Text: ' + txt[:100] + "...")
+txt = '\n'.join(textwrap.wrap(txt_nowrap, 55))
 
 # Add blanks
 txt = 10*"\n" + txt + 10*"\n"
 
-#if os.path.exists('txt.mp3') is False:
-tts = gTTS(text=txt, lang=lang_choice)
-tts.save("txt.mp3")
+if os.path.exists(title + ".mp3") is False:
+  if txt_nowrap != '':
+    tts = gTTS(text=txt_nowrap, lang=lang_choice)
+    tts.save(title + ".mp3")
 
-audio = MP3("txt.mp3")
-audio_length = int(audio.info.length)
+try:
+  audio = MP3(title + ".mp3")
+  audio_length = int(audio.info.length)
+except:
+  pass
 
 # CREATE THE TEXT IMAGE
-clip_txt = TextClip(txt,color='white', align='West', fontsize=24, font='NotoSerifTamil-Bold', method='label')
+clip_txt = TextClip(txt,color='white', align='West', fontsize=20, font='NotoSerifTamil-Bold', method='label')
 
 # SCROLL THE TEXT IMAGE BY CROPPING A MOVING AREA
 txt_speed = 8
@@ -85,38 +93,50 @@ moving_txt = clip_txt.fl(fl, apply_to=['mask'])
 background_image = ImageClip('background.jpg')
 background_darkened = background_image.fl_image(lambda pic: (0.6*pic).astype('int16'))
 
-# COMPOSE THE MOVIE
-clips = []
+
 #clips.append(background_image)
 print('Number of images:' + str(len(list_images)))
 list_images_new = []
+list_images_new.append('background.jpg')
 for i in range (len(list_images)):
   filepath = 'tmp/' + list_images[i]
   size = int(os.stat(filepath).st_size)
-  im=Image.open(filepath)
-  if size > 15000 and int(im.size[0]) > 400 :
-    list_images_new.append(list_images[i])
+  width = 0
+  try:
+   im=Image.open(filepath)
+   width = int(im.size[0])
+  except:
+    pass
+  if size > 15000 or width > 400:
+    list_images_new.append(filepath)
 
 
 timing_duration = int(audio_length/len(list_images_new))
 print('Timing Duration:' + str(timing_duration))
 print('New number of images:' + str(len(list_images_new)))
+
+clips = []
 for j in range (len(list_images_new)):
-  #print('tmp/'+list_images_new[j])
-  slide = ImageClip('tmp/'+list_images_new[j]).set_duration(timing_duration).set_start(timing_duration * j).set_pos(lambda t: ('center', 50+t) ).crossfadein(.3)
+  slide = ImageClip(list_images_new[j]).set_duration(timing_duration).set_start(timing_duration * j).set_pos(lambda t: ('center', 50+t) ).crossfadein(.3)
   clips.append(slide)
 
-#clips.append(moving_txt.set_pos(('center','bottom')))
-#slides = [add_transition(len(clips), x, clip) for x, clip in enumerate(clips)]
+clips.append(moving_txt.set_pos(('center','bottom')))
+
 videoclip = CompositeVideoClip(clips, moviesize)
-videoclip.set_duration(audio_length).write_videofile("videoclip_"+ datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ".avi", fps=5, codec='libx264', 
+videoclip.set_duration(audio_length).write_videofile(title + '_' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ".avi", fps=5, codec='libx264', 
         audio="txt.mp3", audio_codec='aac', temp_audiofile='txt.mp3', remove_temp=True
 )
 
+print('Title >> ' + title)
+print('Keywords >> ' + ', '.join(keywords))
+
 # DELETE TEMPORY FILES IMAGES
-filelist = [ f for f in os.listdir("tmp/") if f.endswith(".jpg") ]
-for f in filelist:
+folder = 'tmp'
+for the_file in os.listdir(folder):
+  file_path = os.path.join(folder, the_file)
   try:
-    os.remove(f)
-  except:
-    continue
+    if os.path.isfile(file_path):
+      os.unlink(file_path)
+    elif os.path.isdir(file_path): shutil.rmtree(file_path)
+  except Exception as e:
+    print(e)
