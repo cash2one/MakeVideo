cfd6baa11f363fa9407d844eaec41a399f4541ee
pavnguyen <vunguyen.xbt@gmailc.com>
2017-07-31 13:57:38 +0700

    ok

--- make_video.py
+++ make_video.py
@@ -12,7 +12,7 @@ import urllib.request
 from moviepy.editor import *
 import textwrap
 from moviepy.video.tools.drawing import color_gradient
-import numpy as np
+#import numpy as np
 from skimage import transform as tf
 
 
@@ -22,18 +22,18 @@ H = 720
 moviesize = W, H
 lang_choice = 'en-us'
 if len(sys.argv) > 1:
-  from newspaper import Article
-  url = sys.argv[1]
-  try:
-    lang_choice = sys.argv[2]
-  except:
-    pass
-  print('Language: ' + lang_choice)
-  article = Article(url)
+    from newspaper import Article
+    url = sys.argv[1]
+    try:
+        lang_choice = sys.argv[2]
+    except:
+        pass
+    print('Language: ' + lang_choice)
+    article = Article(url)
 else:
-  import newspaper
-  paper = newspaper.build('http://cnn.com', memoize_articles=False)
-  article = paper.articles[0]
+    import newspaper
+    paper = newspaper.build('http://cnn.com', memoize_articles=False)
+    article = paper.articles[0]
 article.download()
 article.parse()
 authors = article.authors
@@ -44,41 +44,40 @@ images = article.images
 list_images = []
 j = 0
 for i in images:
-  try:
-    j += 1
-    filename = str(j) + '.jpg'
-    urllib.request.urlretrieve(i, 'tmp/' + filename)
-    list_images.append(filename)
-  except:
-    if j > 0:
-      j =- 1
-    continue
+    try:
+        j += 1
+        filename = str(j) + '.jpg'
+        urllib.request.urlretrieve(i, 'tmp/' + filename)
+        list_images.append(filename)
+    except:
+        if j > 0:
+            j =- 1
+        continue
 
-#print(list_images)
 try:
-  article.nlp()
+    article.nlp()
 except:
     print('Error NLP')
     pass
 keywords = article.keywords
 
 txt_nowrap = article.text
-print('Text: ' + txt[:100] + "...")
+print('Text: ' + txt_nowrap[:100] + "...")
 txt = '\n'.join(textwrap.wrap(txt_nowrap, 55))
 
 # Add blanks
 txt = 10*"\n" + txt + 10*"\n"
 
 if os.path.exists(title + ".mp3") is False:
-  if txt_nowrap != '':
-    tts = gTTS(text=txt_nowrap, lang=lang_choice)
-    tts.save(title + ".mp3")
+    if txt_nowrap != '':
+        tts = gTTS(text=txt_nowrap, lang=lang_choice)
+        tts.save(title + ".mp3")
 
 try:
-  audio = MP3(title + ".mp3")
-  audio_length = int(audio.info.length)
+    audio = MP3(title + ".mp3")
+    audio_length = int(audio.info.length)
 except:
-  pass
+    pass
 
 # CREATE THE TEXT IMAGE
 clip_txt = TextClip(txt,color='white', align='West', fontsize=20, font='NotoSerifTamil-Bold', method='label')
@@ -99,16 +98,16 @@ print('Number of images:' + str(len(list_images)))
 list_images_new = []
 list_images_new.append('background.jpg')
 for i in range (len(list_images)):
-  filepath = 'tmp/' + list_images[i]
-  size = int(os.stat(filepath).st_size)
-  width = 0
-  try:
-   im=Image.open(filepath)
-   width = int(im.size[0])
-  except:
-    pass
-  if size > 15000 or width > 400:
-    list_images_new.append(filepath)
+    filepath = 'tmp/' + list_images[i]
+    size = int(os.stat(filepath).st_size)
+    width = 0
+    try:
+        im=Image.open(filepath)
+        width = int(im.size[0])
+    except:
+        pass
+    if size > 15000 or width > 400:
+        list_images_new.append(filepath)
 
 
 timing_duration = int(audio_length/len(list_images_new))
@@ -117,14 +116,14 @@ print('New number of images:' + str(len(list_images_new)))
 
 clips = []
 for j in range (len(list_images_new)):
-  slide = ImageClip(list_images_new[j]).set_duration(timing_duration).set_start(timing_duration * j).set_pos(lambda t: ('center', 50+t) ).crossfadein(.3)
-  clips.append(slide)
+    slide = ImageClip(list_images_new[j]).set_duration(timing_duration).set_start(timing_duration * j).set_pos(lambda t: ('center', 50+t) ).crossfadein(.3)
+    clips.append(slide)
 
 clips.append(moving_txt.set_pos(('center','bottom')))
 
 videoclip = CompositeVideoClip(clips, moviesize)
-videoclip.set_duration(audio_length).write_videofile(title + '_' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ".avi", fps=5, codec='libx264', 
-        audio="txt.mp3", audio_codec='aac', temp_audiofile='txt.mp3', remove_temp=True
+videoclip.set_duration(audio_length).write_videofile(title + '_' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ".avi", 
+                        fps=5, codec='libx264', audio="txt.mp3", audio_codec='aac', temp_audiofile='txt.mp3', remove_temp=True
 )
 
 print('Title >> ' + title)
@@ -133,10 +132,10 @@ print('Keywords >> ' + ', '.join(keywords))
 # DELETE TEMPORY FILES IMAGES
 folder = 'tmp'
 for the_file in os.listdir(folder):
-  file_path = os.path.join(folder, the_file)
-  try:
-    if os.path.isfile(file_path):
-      os.unlink(file_path)
-    elif os.path.isdir(file_path): shutil.rmtree(file_path)
-  except Exception as e:
-    print(e)
+    file_path = os.path.join(folder, the_file)
+    try:
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path): shutil.rmtree(file_path)
+    except Exception as e:
+        print(e)
